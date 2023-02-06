4ac042f4da01706893bc0247bb06ce81ef75ed11
MaxwellSorenGottesman <maxgman16@yahoo.com>
2023-02-05 22:47:57 -0500

    commit tree f4bf03388ae72b9b81a8ae9d814dcbbc1969151b

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,12 +1,21 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, useParams, useRouteMatch } from "react-router-dom";
 import MoviesList from "./MoviesList";
+import MovieShow from "./MovieShow";
 
 function MoviesPage({ movies }) {
+  const match = useRouteMatch();
+
   return (
     <div>
       <MoviesList movies={movies} />
+      <Route exact path={match.url}>
+        <h3>Select a movie to see more info!</h3>
+      </Route>
+      <Route path={`${match.url}/:id`}>
+        <MovieShow movies={movies}/>
+      </Route>
     </div>
-  );
+  )
 }
 export default MoviesPage;
